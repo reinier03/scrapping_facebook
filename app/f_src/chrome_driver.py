96803d2877b6dcc865b2ab88a19999ff2bbfdad3
unknown <reiniermayeargmaill.com>
2025-06-29 13:54:50 -0400

    dfgdfgdg

--- app/f_src/chrome_driver.py
+++ app/f_src/chrome_driver.py
@@ -36,7 +36,7 @@ def anadir_opciones(o, container=False, mobile=False):
     
         
     if container:
-        # o.add_argument("--headless=new")
+        o.add_argument("--headless=new")
         # o.add_argument("--disable-dev-shm-usage")
         # o.add_argument("--disable-gpu")
         pass
