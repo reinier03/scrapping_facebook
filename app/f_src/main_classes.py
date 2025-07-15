106118d700676ae11acaca44bfbf134ec3093585
unknown <reiniermayeargmaill.com>
2025-07-15 14:01:31 -0400

    Classes

--- app/f_src/main_classes.py
+++ app/f_src/main_classes.py
@@ -1,5 +1,7 @@
 from f_src.chrome_driver import uc_driver
 from selenium.webdriver.support.ui import WebDriverWait
+from usefull_functions import *
+import time
 import os
 
 class scrapper:
@@ -13,3 +15,38 @@ class scrapper:
 
     wait_s = WebDriverWait(driver, 8)
     temp_dict = {}
+
+
+
+class Usuario:
+
+    def __init__(self, id, telegram_id):
+        self.id = id
+        self.telegram_id = telegram_id
+        self.folder = user_folder(telegram_id)
+        self.publicaciones: list[Publicacion] = None
+        self.cookies: list[Cookies] = None
+        self.temp_dict = {}
+
+
+    def __setattr__(self, name, value):
+        self.temp_dict[name] = value
+
+class Cookies:
+
+    def __init__(self, id_usuario, cookies_list, cookies_path):
+        self.cookies_dict = cookies_list
+        self.id_usuario = id_usuario
+        self.cookies_path = cookies_path
+        self.cuentas = None
+
+
+
+class Publicacion:
+
+    def __init__(self, id_usuario: int, texto: str, adjuntos: list, titulo: str):
+        self.id_usuario = id_usuario
+        self.texto = texto
+        self.adjuntos = adjuntos
+        self.titulo = titulo
+        self.id = "p{}-{}".format(id_usuario, time.time())
